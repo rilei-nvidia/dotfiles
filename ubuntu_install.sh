a70b12ef81c0b0b295d6e7c4003b4979bc5d1788
Richard Lei <rilei@nvidia.com>
2026-07-10 15:09:57 +1200

    Also install xclip

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -21,7 +21,7 @@ echo "Installing tmux configuration"
 cp -v $(dirname "$0")/.tmux.conf ~/.tmux.conf
 
 echo "Installing apt packages"
-sudo apt install ripgrep
+sudo apt install ripgrep xclip
 
 echo "Done!"
 echo "Reminders:"
